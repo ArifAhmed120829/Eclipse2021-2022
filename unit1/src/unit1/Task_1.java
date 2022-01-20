06f7ed4d4194c47895a6987c7dc5e038578c1c87
ArifAhmed <arif15-12301@diu.edu.bd>
2022-01-20 20:58:48 +0600

    New_Task

---
+++ unit1/src/unit1/Task_1.java
@@ -0,0 +1,34 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Task_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner in = new Scanner(System.in);
+		
+		for(int i = 0;i<5;i++) {
+			double x = Math.random();
+			double y = in.nextDouble();
+			if(x==y) {
+				System.out.println("Congratulations You won");
+				break;
+			}
+			else if(x>y) {
+				System.out.println("Pls input higher value");
+			}
+			else if(y>x) {
+				System.out.println("Pls input lower value");
+				
+			}
+			
+		}
+		System.out.println("Game Over");
+		
+		
+
+	}
+
+}
