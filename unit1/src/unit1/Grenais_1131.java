10560bfe04056de717043fbc3311b7d0554bbcc9
ArifAhmed <arif15-12301@diu.edu.bd>
2022-01-20 18:12:13 +0600

    created a java project and a Unit1 Folder

---
+++ unit1/src/unit1/Grenais_1131.java
@@ -0,0 +1,14 @@
+package unit1;
+
+public class Grenais_1131 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String abul = "I love you";
+		for(int i = 0; i < 10 ; i++) {
+			System.out.println(abul);
+		}
+
+	}
+
+}
