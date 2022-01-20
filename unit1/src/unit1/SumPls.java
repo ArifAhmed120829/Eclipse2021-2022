29dc8f32a4cd875deaab00f1ae89bb3baf219799
ArifAhmed <arif15-12301@diu.edu.bd>
2022-01-20 18:15:24 +0600

    Made another file

---
+++ unit1/src/unit1/SumPls.java
@@ -0,0 +1,15 @@
+package unit1;
+
+public class SumPls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = 20;
+		int sum = x+y;
+		
+		System.out.println("The sum of this two numbers are: "+sum);
+
+	}
+
+}
