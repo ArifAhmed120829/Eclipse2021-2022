415531f2495fc8a154ad6b91faccda3e1b66a10b
ArifAhmed <arif15-12301@diu.edu.bd>
2022-02-02 18:11:40 +0600

    Update Array_Practice.java
    :( not good

--- unit1/src/unit1/Array_Practice.java
+++ unit1/src/unit1/Array_Practice.java
@@ -1,8 +1,14 @@
 package unit1;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
+
 
 public class Array_Practice {
+	static int max;
+	static int min;
+	/*
 	public static boolean simillar(int[]arr, int item) {
 		for(int n:arr) {
 			if(item==n) {
@@ -12,6 +18,26 @@ public class Array_Practice {
 		return false;
 		
 	}
+	*/
+	/*public static int index(int[] arr2, int t) {
+		if(arr2==null)
+			return -1;
+		int i = 0;
+		while(i<arr2.length) {
+			if(t==arr2[i]) return i;
+			else i = i + 1;
+		}
+		return -1;
+		
+	}
+	*/
+	
+		
+
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -31,10 +57,82 @@ public class Array_Practice {
 		}
 		double avg = sum/numbers.length;
 		System.out.println(avg);
-		*/
+		
 		int[]arr2 = {112,254,366,412,55,995,201,103,104,100};
 		System.out.println(simillar(arr2,201));
 		System.out.println(simillar(arr2,103));
+		
+		int[] arr = {10,20,30,40,50};//5
+		System.out.println("Original Array: "+Arrays.toString(arr));
+		
+		int remove = 1;
+		for(int i = remove; i<arr.length-1; i++) {//1<4
+			arr[i] = arr[i+1];//20 = 30, 30 = 40, 40 = 50, 50 = 50,
+		}
+		System.out.println(Arrays.toString(arr));
+		
+		int arr[] = {10,20,3,0,4,1,2,5,6};
+		int[] arr2 = new int[9];
+		System.out.println("Previous arr: "+Arrays.toString(arr));
+		for(int i = 0; i < arr2.length; i++) {
+			arr2[i] = arr[i];
+		}
+		System.out.println("Copy array: "+Arrays.toString(arr2));
+		
+		int[] lau = {100,200,300,400,500,600,700,800,900,1000};
+		//Pls change the index no 3
+		int indexchng = 3;
+		int new_value = 90000;
+		System.out.println(Arrays.toString(lau));
+		for(int i = lau.length-1; i>indexchng; i--) {//loop er man orthat se kotobar ghurbe
+			lau[i] = lau[i-1];//real man paise akhane 900 = 800, 800 = 700
+			
+		}
+		lau[indexchng] = new_value;//lau(3) = lau(90000)
+		System.out.println("newArr: "+Arrays.toString(lau));
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
 	}
 
 }
