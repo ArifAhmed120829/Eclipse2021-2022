e9d73a70b51c8180b0a61052da73169fc181376f
ArifAhmed <arif15-12301@diu.edu.bd>
2022-02-02 21:13:32 +0600

    Solved
    :+1:

---
+++ unit1/src/unit1/Array_Fill_II.java
@@ -0,0 +1,29 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Array_Fill_II {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();//3
+		int[] N = new int[1000];
+		for(int i=0,j=0; i<N.length; i++) {
+			
+			System.out.println("N["+i+"] = "+j);
+			j++;
+			if(j==T)
+				j=0;
+			
+		}
+			
+		
+				
+				
+		
+	
+		
+	}
+
+}
