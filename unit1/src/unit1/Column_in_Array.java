d7c9bf277da495a9bf528e7b532c11448a0977f6
ArifAhmed <arif15-12301@diu.edu.bd>
2022-02-08 21:52:01 +0600

    Solved
    2 problem solved

---
+++ unit1/src/unit1/Column_in_Array.java
@@ -0,0 +1,38 @@
+package unit1;
+
+import java.util.Scanner;
+
+public class Column_in_Array {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		double[][] M = new double[12][12];
+		int L = in.nextInt();
+		int r,c;
+		double sum = 0.0;
+		char[] T;
+		T = new char[2];
+		T = in.next().toCharArray();
+		for( r = 0; r<12;r++) {
+			for(c = 0; c<12;c++) {
+					M[r][c] = in.nextDouble();
+			}
+		}
+		for(r = 0; r<12; r++) {
+			sum = sum + M[r][L];
+			
+		}
+		if(T[0]=='S') {
+			String str = String.format("%.1f", sum);
+			System.out.println(str);
+		}
+		else if(T[0]=='M') {
+			double res = sum/12;
+			String str = String.format("%.1f", res);
+			System.out.println(str);
+		}
+
+	}
+
+}
